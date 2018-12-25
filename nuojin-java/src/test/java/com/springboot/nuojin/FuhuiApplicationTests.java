ee9bae1db96c2fc64f62dd2c8e3c742b9f5dbd22
inwsy <inwsy@hotmail.com>
2018-12-25 11:31:11 +0800

    update

--- nuojin-java/src/test/java/com/springboot/nuojin/FuhuiApplicationTests.java
+++ nuojin-java/src/test/java/com/springboot/nuojin/FuhuiApplicationTests.java
@@ -1,16 +1,14 @@
+package com.springboot.nuojin;
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.springboot.nuojin.RSAUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
@@ -76,9 +74,5 @@ public class FuhuiApplicationTests {
 //
 //        System.out.println(result);
     }
-
-
-
-
 
 }
